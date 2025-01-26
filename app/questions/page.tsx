edd7cc67c5285742286a6cf406e40c5a4f3fb724
Sathya Sriram <98763491+sathyasriram777@users.noreply.github.com>
2025-01-25 20:38:32 -0500

    things

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,43 +1,66 @@
-import { title } from "@/components/primitives";
+"use client";
+
 import { Input } from "@heroui/input";
 import { Slider } from "@heroui/slider";
-import { Select, SelectItem } from "@heroui/select";
+import React from "react";
+import { Listbox, ListboxItem } from "@heroui/listbox";
+import { ReactNode } from "react";
 
+export const ListboxWrapper = ({ children }: { children: ReactNode }) => (
+  <div className="w-[260px] border-small px-1 py-2 rounded-small border-default-200 dark:border-default-100">
+    {children}
+  </div>
+);
 
-export const animals = [
-  { key: "cat", label: "Cat" },
-  { key: "dog", label: "Dog" },
-  { key: "elephant", label: "Elephant" },
-  { key: "lion", label: "Lion" },
-  { key: "tiger", label: "Tiger" },
-  { key: "giraffe", label: "Giraffe" },
-  { key: "dolphin", label: "Dolphin" },
-  { key: "penguin", label: "Penguin" },
-  { key: "zebra", label: "Zebra" },
-  { key: "shark", label: "Shark" },
-  { key: "whale", label: "Whale" },
-  { key: "otter", label: "Otter" },
-  { key: "crocodile", label: "Crocodile" },
-];
+export default function QuestionsPage() {
+  const [selectedKeys, setSelectedKeys] = React.useState(
+    new Set(["early bird"]),
+  );
 
-export default function PricingPage() {
+  const selectedValue = React.useMemo(
+    () => Array.from(selectedKeys).join(", "),
+    [selectedKeys],
+  );
 
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">What are your personal goals?</h3>
-        <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-          
+        <h3 className="text-default-500 text-large">
+          Are you an early bird or a night owl?
+        </h3>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-center">
+            <ListboxWrapper>
+              <Listbox
+                disallowEmptySelection
+                aria-label="Single selection example"
+                selectedKeys={selectedKeys}
+                selectionMode="single"
+                variant="flat"
+                onSelectionChange={(keys) =>
+                  setSelectedKeys(keys as Set<string>)
+                }
+              >
+                <ListboxItem key="early bird">Early Bird</ListboxItem>
+                <ListboxItem key="night owl">Night Hedwig</ListboxItem>
+              </Listbox>
+            </ListboxWrapper>
+          </div>
+          <p className="text-small text-default-500">
+            Selected value: {selectedValue}
+          </p>
         </div>
       </div>
 
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">What are your personal goals?</h3>
+        <h3 className="text-default-500 text-small">
+          What are your personal goals?
+        </h3>
         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
           <Input
             key={"outside"}
             // description={"outside"}
-            label="Email"
+            label="Enter your response here"
             // labelPlacement={"outside"}
             type="text"
           />
@@ -45,22 +68,24 @@ export default function PricingPage() {
       </div>
 
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">Which of the following describes your academic status the best?</h3>
+        <h3 className="text-default-500 text-small">
+          Which of the following describes your academic status the best?
+        </h3>
         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-
           <Input
             key={"outside"}
             // description={"outside"}
-            label="Email"
+            label="Enter your response here"
             // labelPlacement={"outside"}
             type="text"
           />
         </div>
       </div>
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">How much social time would you be able to allocate between classes?</h3>
+        <h3 className="text-default-500 text-small">
+          How much social time would you be able to allocate between classes?
+        </h3>
         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-
           <Slider
             aria-label="Temperature"
             className="max-w-md"
@@ -73,23 +98,48 @@ export default function PricingPage() {
         </div>
       </div>
 
-
       <div className="flex flex-col gap-2">
-        <h3 className="text-default-500 text-small">Rate your course load on the scale below</h3>
+        <h3 className="text-default-500 text-small">
+          Rate your course load on the scale below
+        </h3>
         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
-
           <Input
             key={"outside"}
             // description={"outside"}
-            label="Email"
+            label="Enter your response here"
             // labelPlacement={"outside"}
             type="text"
           />
         </div>
       </div>
-
-
-
     </div>
   );
 }
+
+// export default function App() {
+//   const placements = ["inside", "outside", "outside-left"];
+
+//   return (
+//     <div className="w-full flex flex-col gap-4">
+//       <div className="flex flex-col gap-2"></div>
+//       <div className="flex flex-col gap-2">
+//         <h3 className="text-default-500 text-small">With placeholder</h3>
+//         <div className="flex w-full flex-wrap items-end md:flex-nowrap mb-6 md:mb-0 gap-4">
+//           {placements.map((placement) => (
+//             <Select
+//               key={placement}
+//               className="max-w-xs"
+//               label="Favorite Animal"
+//               labelPlacement={placement}
+//               placeholder="Select an animal"
+//             >
+//               {animals.map((animal) => (
+//                 <SelectItem key={animal.key}>{animal.label}</SelectItem>
+//               ))}
+//             </Select>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
